290832e9ed5e38f07bc32986b77cd465781d82df
Daniela Alonso <alonsod2000@gmail.com>
2024-06-25 22:54:42 -0700

    router

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TodoList from './components/ToDoList';
-import './App.css'
-function App() {
- return (
- <div className="App">
- <TodoList />
- </div>
- ); 
+
+class App extends React.Component {
+  render() {
+    console.log(this.state);
+    console.log("This is the process.env", process.env.PUBLIC_URL);
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <div>
+          <Routes>
+            <Route path='/' element={<TodoList />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
+
 export default App;
